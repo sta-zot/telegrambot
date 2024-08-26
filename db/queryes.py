f5911d38b1b47817ede573f6d8a82de3ce0e9fa6
Stanislav <zss1988@ya.ru>
2024-08-26 17:21:48 +0300

    a

--- db/queryes.py
+++
@@ -1,9 +0,0 @@
-
-"""
-The module describes the data management functions in the application database
-
-В модуле описываются функции по управлению данными в базе данных приложения
-"""
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
-from sqlalchemy import select, insert, update, delete
-from configuration import config
